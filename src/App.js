413bb91efd1ab78f2551fbca4317516e62cbcb53
Chenxi Cai <s205420@dtu.dk>
2022-07-26 16:21:27 +0200

    submitted reservation to firebase succesfully

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React, { useState,useEffect } from 'react';
 import {db} from './firebase-connection';
 import {collection, getDocs ,addDoc, updateDoc, doc, deleteDoc} from 'firebase/firestore';
-import { Navbar } from './Components/Navbar';
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import { Layout } from './pages/Layout';
 import { AboutUS } from './pages/AboutUS';
