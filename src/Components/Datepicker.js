a8ee6f764bc9197de7fbc2f7874775f52f945b52
Chenxi Cai <s205420@dtu.dk>
2022-07-26 16:22:06 +0200

    added usestates

--- src/Components/Datepicker.js
+++ src/Components/Datepicker.js
@@ -1,34 +1,65 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css_files/datepicker.css';
 import Popup from 'reactjs-popup';
 import { GoCheck } from 'react-icons/go'
+import {db} from '../firebase-connection';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
 
 export const Datepicker = () => {
-    const [date,setDate]=useState();
-    const [time,setTime]=useState();
-    const [number,setNumber]=useState();
+
+  const [newNumber,setNewNumber] =useState(0);
+  const [newTime,setNewTime] =useState(0);
+  const [newDate,setNewDate] = useState(Date());
+  const reservationCollectionRef = collection(db,"reservations");
+
+    const [reservation,setReservation] = useState([]);
+    const createReservation = async () =>{
+      await addDoc(reservationCollectionRef, {numberOfGuests: Number(newNumber), date: newDate, time: newTime});
+    }
+    const popup =  () => {
+       window.alert("Your Reservation at Is Confirmed")
+    };
+
+   useEffect(() =>{
+      const getUsers = async () => {
+        const data = await getDocs(reservationCollectionRef);
+        setReservation(data.docs.map((doc) => ({...doc.data(),id: doc.id})));
+      };
+      
+      getUsers();
+   },[]);
+
+
 
   return (
 
     <div className='main'>
-        <h1>Selected number of guests: {number} </h1>
-        <input type="number" onChange={e=>setNumber(e.target.value)} min="1" step="1"/>
-        <p className='text10'>Please contact us if the number are more than 10</p>
+      <h1>Please Select Number of Guests</h1>
+      <input type="number" placeholder="numberOfGuests..." 
+        onChange={(event) => 
+      {setNewNumber(event.target.value);}}/>
+      <p className='text10'>Please Contact Us If You Are More Than 10</p>
 
-        <h1>Selected Date: {date} </h1>
-        <input type="date" onChange={e=>setDate(e.target.value)}/>
+      <h1>Select The Date</h1>
+      <input type ="date" placeholder ="Date..."
+       onChange={(event) => 
+      {setNewDate(event.target.value);}}/>
 
-        <h1>Selected Time: {time} </h1>
-        <input type="time" onChange={e=>setTime(e.target.value)} className="timeinput"/>
-        <p></p>
-
-
-        <Popup trigger={<button className='confirmbutton'> 
-            Confirm The Reservation</button>} position="bottom center">
-            <div className='buttontext'>Your Booking Has Been Confirmed <GoCheck className='checkicon'/></div>
-         </Popup>
-    
+      <h1>Select The Time</h1>
+       <input type ="time" placeholder ="Time..." className='timeinput'
+         onChange={(event) => 
+      {setNewTime(event.target.value);}}/>
+      <p></p>
+          
+   <button    onClick={() => {
+          createReservation();
+          popup();
+        }} className="confirmbutton">
+    Confirm Reservation
+    </button>
+          
     </div>
+      
     
   )
 }
